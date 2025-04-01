57bd46a525510be4c16003cd8a3b60277e1eac3c
BradI. <bradleyike5187@gmail.com>
2025-03-31 20:03:25 -0700

    Men-Stack-CRUD-App-Project-2

---
+++ controllers/workouts.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+
+router.get('/', (req, res) => {
+  const user = req.session.user; // Assuming user is stored in the session
+  res.render('home', { user });
+});
+
+
+
+//index route
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('workouts/index.ejs', {
+          workouts: currentUser.workouts,
+        });
+      } catch (error) {
+        console.log(error);
+        res.redirect('/');
+      }
+    }); 
+
+
+// New workout route
+  router.get('/new', async (req, res) => {
+    res.render('workouts/new.ejs');
+  });
+
+  // Create workout route
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.workouts.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/workouts`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+  
+// Show workout route
+router.get('/:workoutId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const workout = currentUser.workouts.id(req.params.workoutId);
+    res.render('workouts/show.ejs', {
+      workout: workout,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+// Delete workout route
+router.delete('/:workoutId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.workouts.id(req.params.workoutId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/workouts`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/:workoutId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const workout = currentUser.workouts.id(req.params.workoutId);
+    res.render('workouts/edit.ejs', {
+      workout: workout,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+
+// Update workout route
+router.put('/:workoutId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const workout = currentUser.workouts.id(req.params.workoutId);
+    workout.set(req.body);
+    await currentUser.save();
+    res.redirect(
+      `/users/${currentUser._id}/workouts/${req.params.workoutId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const workoutSchema = new mongoose.Schema({
+ 
+  workoutType: {
+    type: String,
+    required: true,
+    enum: ["Running", "Weightlifting", "Cycling"],
+  },
+  workoutDescription: {
+    type: String,
+    required: true,
+  },
+  workoutDuration: {
+    type: Number,
+    required: true,
+    min: 5,
+    max: 60,
+  },
+  postingLink: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+    workouts: [workoutSchema],
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
